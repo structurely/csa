27a33a16c624011c85876e61f6c191e1c924aa7f
Boudhayan Banerjee <boudhayanbanerjee@gmail.com>
2017-06-29 10:39:46 -0500

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from distutils.core import setup, Extension
 from Cython.Build import cythonize
-
-ext_modules = [Extension("csa",
+try:
+    from setuptools import setup,Extension
+except ImportError:
+    from distutils.core import setup,Extension
+    
+ext_modules = [Extension("pycsa",
                          sources=["csa/optimize.pyx"],
-                         libraries=["m"])]
+                         libraries=["m"])]   
 setup(
-    name="csa",
-    packages=["csa"],
-    package_dir={"csa": "csa"},
-    ext_modules=cythonize(ext_modules)
+    name="pycsa",
+    version="0.1.2",
+    description="Python implementation of coupled simulated annealing (CSA)",
+    long_description=open("README.rst").read(),
+    author="Boudhayan Banerjee, Evan 'Pete' Walsh",
+    author_email="ronnie@structurely.com, epwalsh@structurely.com",
+    url="https://github.com/structurely/csa",
+    packages=["pycsa"],
+    package_dir={"pycsa": "csa"},
+    ext_modules=cythonize(ext_modules),
+    keywords=[
+        "csa",
+        "sa",
+        "simulated annealing",
+        "annealing",
+        "coupled simulated annealing",
+    ],
+    classifiers=[],
 )
