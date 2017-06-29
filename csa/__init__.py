8f786c02a895d0f81294d4eac28d15f153440396
Boudhayan Banerjee <boudhayanbanerjee@gmail.com>
2017-06-29 10:40:29 -0500

    update

--- csa/__init__.py
+++ csa/__init__.py
@@ -1,8 +1,3 @@
 from __future__ import absolute_import
-from .algorithm import CoupledAnnealer
 
-__all__ = ['CoupledAnnealer']
-__author__ = "Evan Pete Walsh"
-__license__ = "MIT"
-__email__ = "epwalsh@structurely.com"
-__version__ = "1.0"
+from .algorithm import CoupledAnnealer
